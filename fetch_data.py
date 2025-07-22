d08254ba5b5c5cc654e5b37c34dfda64beaebf32
IamMegaNoobb <unknownpoom@gmail.com>
2025-07-22 19:25:18 +0700

    modified api key for iqair

--- fetch_data.py
+++ fetch_data.py
@@ -127,5 +127,5 @@ provinces = ['Amnat Charoen', 'Ang Thong', 'Bangkok', 'Buriram', 'Chachoengsao',
 # print(json.dumps(forecast_data, indent=2, ensure_ascii=False))
 
 if __name__ == "__main__":
-    fetch_all_data('0199d98b-12a7-4ea8-8ef8-674a08a79ce7', provinces, 'w')
+    fetch_all_data('c276b727-085b-4e13-8a72-181ce160c0a6', provinces, 'w')
     get_forecast('2eb93a22c78a1235a2f52546750a691ba41cd200', provinces, 'w')
