d510df6b200fd3668640d60f715500c4acc3b18f
IamMegaNoobb <unknownpoom@gmail.com>
2025-07-22 19:12:15 +0700

    added .github and fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -125,3 +125,6 @@ provinces = ['Amnat Charoen', 'Ang Thong', 'Bangkok', 'Buriram', 'Chachoengsao',
 # aqicn
 # forecast_data = get_forecast('2eb93a22c78a1235a2f52546750a691ba41cd200', provinces, 'w')
 # print(json.dumps(forecast_data, indent=2, ensure_ascii=False))
+
+fetch_all_data('0199d98b-12a7-4ea8-8ef8-674a08a79ce7', provinces, 'w')
+get_forecast('2eb93a22c78a1235a2f52546750a691ba41cd200', provinces, 'w')
